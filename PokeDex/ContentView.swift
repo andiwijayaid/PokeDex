db0c1b9ff02144d4e1d9d451e6662dbdf777b2b1
Andi Wijaya <andiwijayaid@gmail.com>
2020-06-24 11:49:20 +0800

    Initial Commit

---
+++ PokeDex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeDex
+//
+//  Created by Andi Wijaya on 24/06/20.
+//  Copyright © 2020 Andi Wijaya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
